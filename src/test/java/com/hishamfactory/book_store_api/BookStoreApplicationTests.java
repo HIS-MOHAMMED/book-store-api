454da74117376ecac0a04d49081d20f2a22c6249
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-07-07 13:48:08 +0300

    Initial commit

---
+++ src/test/java/com/hishamfactory/book_store_api/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hishamfactory.book_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
