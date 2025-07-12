adf734812fca5e2fef0197a5afee17c74e173714
Hisham Mohammed <hishammohammed1199@gmail.com>
2025-07-12 14:54:35 +0300

    add exception handler and restControllerAdvice to handle exceptions

---
+++ src/main/java/com/hishamfactory/book_store_api/excption/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.hishamfactory.book_store_api.excption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
